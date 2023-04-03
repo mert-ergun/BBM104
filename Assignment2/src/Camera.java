e0d4cefc30a157beb3d5f3d3a9568d5a1a1b5dee
Mert <mertergun17@hotmail.com>
2023-04-03 17:25:24 +0300

    Camera class initials

--- Assignment2/src/Camera.java
+++ Assignment2/src/Camera.java
@@ -1,4 +1,11 @@
 public class Camera extends Smart {
     private int storage;
     private int mbps;
+
+    public Camera(int mbps) {
+        if (mbps < 0) {
+            throw new IllegalArgumentException("Mbps must be greater than 0");
+        } this.mbps = mbps;
+        this.storage = 0;
+    }
 }
