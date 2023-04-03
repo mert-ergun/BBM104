dd99592782e9d9cd0ed9fdadf8b761828e111110
Mert <mertergun17@hotmail.com>
2023-04-03 20:30:59 +0300

    Time Checker first version

--- Assignment2/src/TimeChecker.java
+++ Assignment2/src/TimeChecker.java
@@ -1,3 +1,30 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class TimeChecker {
-    
+    Calendar initDate;
+    Calendar currentDate;
+    Calendar oldDate; 
+
+    public void SetInitialTime(String time) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(formatter.parse(time));
+        initDate = calendar;
+        currentDate = calendar;
+    }
+
+    public void SetTime(String time) throws ParseException {
+        oldDate = currentDate;
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(formatter.parse(time));
+        currentDate = calendar;
+    }
+
+    public void SkipMinutes(int minutes) {
+        oldDate = currentDate;
+        currentDate.add(Calendar.MINUTE, minutes);
+    }
 }
