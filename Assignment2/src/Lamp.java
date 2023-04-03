be2fbee990d42a42115f213797164452364bad11
Mert <mertergun17@hotmail.com>
2023-04-03 17:20:27 +0300

    Lamp Class initial values

--- Assignment2/src/Lamp.java
+++ Assignment2/src/Lamp.java
@@ -1,5 +1,19 @@
 public class Lamp extends Smart {
     private int kelvin, brightness;
+
+    public Lamp() {
+        this.kelvin = 4000;
+        this.brightness = 100;
+    }
+
+    public Lamp(int kelvin, int brightness) {
+        if (kelvin < 2000 || kelvin > 6500) {
+            throw new IllegalArgumentException("Kelvin must be between 0 and 10000");
+        } this.kelvin = kelvin;
+        if (brightness < 0 || brightness > 100) {
+            throw new IllegalArgumentException("Brightness must be between 0 and 100");
+        } this.brightness = brightness;
+    }
 }
 
 class ColoredLamp extends Lamp {
@@ -7,4 +21,18 @@ class ColoredLamp extends Lamp {
     private int decimalColor = Integer.parseInt(color, 16);
     private boolean isColorOn;
     
+    public ColoredLamp() {
+        this.color = "FFFFFF";
+        this.isColorOn = false;
+    }
+
+    public ColoredLamp(String color) {
+        if (color.length() != 6) {
+            throw new IllegalArgumentException("Color must be a 6-digit hexadecimal number");
+        } else if (color.matches("[0-9A-Fa-f]+") == false) {
+            throw new IllegalArgumentException("Color must be a 6-digit hexadecimal number");
+        } 
+        this.color = color;
+        this.isColorOn = true;
+    }
 }
