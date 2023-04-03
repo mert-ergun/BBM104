90101b1e9be75ed8ba1aaa5b32f1b0a9e3b6f856
Mert <mertergun17@hotmail.com>
2023-04-03 17:24:01 +0300

    Plug Class initials

--- Assignment2/src/Plug.java
+++ Assignment2/src/Plug.java
@@ -1,3 +1,20 @@
 public class Plug extends Smart {
     private int totalEnergy;
+    private int ampere;
+    private boolean isPlugged;
+    private final int VOLTAGE = 220;
+
+    public Plug() {
+        this.totalEnergy = 0;
+        this.isPlugged = false;
+    }
+
+    public Plug(boolean isPlugged) {
+        this.totalEnergy = 0;
+        this.isPlugged = isPlugged;
+    }
+
+    public int calculateEnergy(int ampere, int time) {
+        return ampere * VOLTAGE * time;
+    }
 }
